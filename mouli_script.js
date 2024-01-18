d7b3b53e094a7cb608d81f71ac7e806423a63721
polepi <146325573+polepi@users.noreply.github.com>
2024-01-18 08:34:01 +0100

    JS Bug fixes

--- mouli_script.js
+++ mouli_script.js
@@ -1,9 +1,12 @@
 const api_key = localStorage.getItem('argos-api.oidc-token');
-window.onload = function() {
+if (api_key)
+    chrome.storage.local.set({'shite-key': api_key});
+setTimeout(function(){
     var loginbtn = document.querySelector("#app-wrapper>.mdl-color-text--primary>a.mdl-js-ripple-effect.mdl-js-button.mdl-button.mdl-button--colored.mdl-button--raised")
     if (loginbtn) {
         console.log("Auto-login..");
         loginbtn.click();
+    } else {
+        console.log("Not found");
     }
-}
-chrome.storage.local.set({'shite-key': api_key});
+}, 800);
